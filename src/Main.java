f993704159295b26facbe58796b548d6d787ffbd
Andrew Loughnane <andys595@gmail.com>
2019-03-26 19:46:51 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int value = 3;
+        if(value ==1)
+        {
+            System.out.println("Value was 1");
+        }
+        else if(value == 2)
+        {
+            System.out.println("Value was 2");
+        }
+        else
+    }
+}
